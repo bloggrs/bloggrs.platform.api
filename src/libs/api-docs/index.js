96729ebea62d65b038d6212b773634e5f9c244a4
Gjergj Kadriu <gjergjk71@gmail.com>
2020-11-22 12:12:02 +0100

    Improvements to template

--- src/libs/api-docs/index.js
+++ src/libs/api-docs/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = module.exports = express();
 
 const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const path = require('path');
-const swagger_path =  path.resolve(__dirname,'./swagger.yaml');
-const swaggerDocument = YAML.load(swagger_path);
+
+// const swaggerDocument = YAML.load(swagger_path);
+const { DocsCollector } = require("../../../libs/docs-collector");
+const swaggerDocument = DocsCollector.getSwaggerDocument();
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
